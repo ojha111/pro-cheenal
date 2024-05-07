1b2c9770e8a283a34689470129cc2dd3b59a6814
ojha111 <152144787+ojha111@users.noreply.github.com>
2024-05-08 00:27:11 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,3 @@
-api_id = "29403305"
+api_id = 29403305
 api_hash = "54c435573d0f2d9dada270a0cb493b36"
 bot_token = "6751491972:AAFKab49olB8sT0vGCxNKYM-Y3o-tCQ1m-M"
-sudo_users = "6687634412"
-auth_groups = "-1002077871846"
