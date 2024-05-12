014b18f4ec17594fee7c43d2023abd0817e84aaf
ojha111 <152144787+ojha111@users.noreply.github.com>
2024-05-12 08:38:53 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,3 @@
 api_id = 29403305
 api_hash = "54c435573d0f2d9dada270a0cb493b36"
-bot_token = "6751491972:AAFKab49olB8sT0vGCxNKYM-Y3o-tCQ1m-M"
+bot_token = "6955629747:AAGhYrIQrujjC0mOFWoG3MSGvw-wWcHt_i4"
